c2ec12f63e9b14b21d17d04e04405c52b623519b
mohamed <94742483+Mohaamedl@users.noreply.github.com>
2024-06-18 23:44:53 +0100

    connected with database supabase

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,12 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
+
 export default function page() {
   return (
     <>
     <div>
-      <h1>Dashboard
-      </h1>
+      <h1>Dashboard</h1>
+      
+      
       <UserButton afterSignOutUrl="/" />
-        
+      <UserButton signInUrl="/app/(auth)/sign-in/[[...sign-in]]/page.tsx"/>
+      
       
     </div>
     </>
